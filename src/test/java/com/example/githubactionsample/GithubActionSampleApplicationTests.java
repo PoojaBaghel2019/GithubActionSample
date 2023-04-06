9d5b93e1949e4fd268dcf6df4112d0b7764c1ab3
PoojaBaghel2019 <poojasinghstorage21@gmail.com>
2023-04-06 16:37:22 +0530

    Publish Build Scans from GitHub Actions

--- src/test/java/com/example/githubactionsample/GithubActionSampleApplicationTests.java
+++ src/test/java/com/example/githubactionsample/GithubActionSampleApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.githubactionsample;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes=GithubActionSampleApplicationTests.class)
 class GithubActionSampleApplicationTests {
 
     @Test
